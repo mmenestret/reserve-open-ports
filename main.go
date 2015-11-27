e1d262b47255e48c74d2f0208c5e18ec90405c28
txominpelu imediava@gmail.com <imediava@gmail.com>
2015-11-27 17:14:35 +0100

    Prout

--- main.go
+++ main.go
@@ -6,28 +6,34 @@ import (
 	"net"
 )
 
-func accept() bool {
-	ln, err := net.Listen("tcp", ":8081")
+func open_port() bool {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", "localhost:8081")
 	if err != nil {
 		fmt.Println("Error!")
 		return false
 		// handle error
 	}
-	_, err2 := ln.Accept()
-	if err2 != nil {
+	_, err1 := net.ListenTCP("tcp", tcpAddr)
+	if err1 != nil {
 		fmt.Println("Error!")
 		return false
+		// handle error
 	}
 	return true
 }
 
-func bookPort(min int, max int) {
+func bookPort(min int, max int) bool {
 	//currentPort := min
-	fmt.Println("Success!")
+	//has_port := false
+	//for !has_port {
+	return open_port()
+	//}
 }
 
 func main() {
 	flag.Parse()
-	bookPort(8080, 8090)
+
+	port_is_open := bookPort(8080, 8090)
 	fmt.Println("Finished!")
+	fmt.Println(port_is_open)
 }
